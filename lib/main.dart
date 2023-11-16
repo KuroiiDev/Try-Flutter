51eebb785fe71ae1213c846009bc059b9e182138
KuroiiDev <maitsamkadzim26@gmail.com>
2023-11-16 14:11:28 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter 1',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -5,7 +5,58 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page", style:TextStyle(color: Colors.white)),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              CustomMenu(imageAsset: 'circle', title: 'Circle'),
+              CustomMenu(imageAsset: 'square', title: 'Square'),
+            ],
+          ),
+          Row(
+            children: [
+              CustomMenu(imageAsset: 'triangle', title: 'Triangle'),
+              CustomMenu(imageAsset: 'rectangle', title: 'Rectangle'),
+            ],
+          ),
+          Row(
+            children: [
+              CustomMenu(imageAsset: 'pentagon', title: 'Hexagon'),
+              CustomMenu(imageAsset: 'hexagon', title: 'Hexagon'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAsset, required this.title,
+  });
+  final String imageAsset;
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 2,vertical: 2),
+          color: Color(0xff282c34),
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+            children: [
+              Image.asset(('assets/'+imageAsset+".png"), height: 200),
+              Text(title, style:TextStyle(color: Colors.white)),
+            ],
+          ),
+      ),
+    );
   }
 }
 
